91ed38185c75c3a22d3106af4e02f1ecf6dc8d87
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-11-17 14:30:45 -0400

    flask db

--- intro_to_flask/flaskr/app.py
+++ intro_to_flask/flaskr/app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "e77ef067f5a4882c4821cec3cafc7001adc249327406890e"
+db = SQLAlchemy(app)
+migrate = Migrate(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    is_complete = db.Column(db.Boolean, nullable=False)
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    migrate
+    print("Initialized the database.")
 
 
 @app.route("/")
