b3de20050722842b9958f697b4ad4296f7c99599
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2026-02-12 21:31:13 -0400

    Update index.ts

--- express_biginners/src/index.ts
+++ express_biginners/src/index.ts
@@ -3,8 +3,34 @@ import express, { Response, Request, NextFunction } from "express";
 const app = express();
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("This is a response");
+  res.send("Express fundamentals");
 });
+
+app.get("/json", (req: Request, res: Response, next: NextFunction) => {
+  const data = {
+    name: "Addison",
+    location: "Dominican Republic",
+  };
+  res.json(data);
+});
+
+app.get("/html", (req: Request, res: Response, next: NextFunction) => {
+  const html = "<html><h1>This is a HTML response</h1></html>";
+  res.send(html);
+});
+
+app.get("/query", (req: Request, res: Response, next: NextFunction) => {
+  const query = req.query;
+  res.json(query);
+});
+
+app.get(
+  "/params/:name/:location/:occupation",
+  (req: Request, res: Response, next: NextFunction) => {
+    const params = req.params;
+    res.json({ params });
+  },
+);
 
 app.listen("3000", () => {
   console.log("Server listening on http://localhost:3000");
