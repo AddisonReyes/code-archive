a8d42a323474b7be4631b5608afac11e729b17d6
AddisonReyes <addison.amin@gmail.com>
2026-02-26 09:38:09 -0400

    Recipes form and new template

--- recipe_social_site/app/routes.py
+++ recipe_social_site/app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 
-from . import app
+from . import app, db
+from .forms import RecipeForm
 from .models import Recipe
 
 
@@ -8,3 +9,17 @@ from .models import Recipe
 def home():
     recipes = Recipe.query.all()
     return render_template("home.html", recipes=recipes)
+
+
+@app.route("/new_recipe", methods=["GET", "POST"])
+def new_recipe():
+    form = RecipeForm()
+    if form.validate_on_submit():
+        recipe = Recipe(
+            title=form.title.data,
+            description=form.description.data,
+        )
+        db.session.add(recipe)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("new_recipe.html", title="New Recipe", form=form)
