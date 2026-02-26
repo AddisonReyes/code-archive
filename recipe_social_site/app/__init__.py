7ee09cc4bc8046012b2a7916a66e11043a4fc745
AddisonReyes <addison.amin@gmail.com>
2026-02-26 09:58:37 -0400

    Adding users model and new libreries

--- recipe_social_site/app/__init__.py
+++ recipe_social_site/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,5 +9,8 @@ app.config["SECRET_KEY"] = "ecbe91bafa0fb5aea888f3ad22dec1f4cf4e9001ebe23c2c"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 from app import models, routes  # noqa
--- recipe_social_site/app/models.py
+++ recipe_social_site/app/models.py
@@ -1,3 +1,6 @@
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from . import db
 
 
@@ -5,6 +8,24 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def __repr__(self):
         return f"<Recipe {self.title}>"
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    recipes = db.relationship("Recipe", backref="author", lazy="dynamic")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
