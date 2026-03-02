4d6225aa1658f6684cf16725138a981ddcd52478
AddisonReyes <addison.amin@gmail.com>
2026-03-01 23:35:09 -0400

    Register template

--- recipe_social_site/app/routes.py
+++ recipe_social_site/app/routes.py
@@ -1,8 +1,9 @@
 from flask import redirect, render_template, url_for
+from flask_login import current_user
 
 from . import app, db
 from .forms import RecipeForm
-from .models import Recipe
+from .models import Recipe, RegistrationForm, User
 
 
 @app.route("/")
@@ -23,3 +24,21 @@ def new_recipe():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("new_recipe.html", title="New Recipe", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
