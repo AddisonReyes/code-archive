f43a86170e15297c9670cf8a90864699675feaf0
AddisonReyes <addison.amin@gmail.com>
2026-03-08 06:27:52 -0400

    r

--- web_apps_with_react19/learn_react/src/App.jsx
+++ web_apps_with_react19/learn_react/src/App.jsx
@@ -1,10 +1,39 @@
+import { CreateElement } from "react";
+
+import Message from "./Message";
 import Header from "./Header";
 
+const title = "Welcome to React!";
+
 function App() {
+  const name = "Addison";
+
+  const formatMessage = (name) => {
+    return `Hello, ${name}!`;
+  };
+
+  const headingStyle = {
+    color: "teal",
+    fontSize: "28px",
+    textAlign: "center",
+    margin: "20px 0",
+  };
+
   return (
     <div>
       <Header />
-      <h1>Hello, React!</h1>
+      <h1>{title}</h1>
+      <Message text="This is a message component." />
+      <Message text="This is another message component." />
+
+      {/* Using a JavaScript variable */}
+      <p>Me: {name}</p>
+
+      {/* Using a function  */}
+      <p>{formatMessage(name)}</p>
+
+      {/* Inline styles */}
+      <h2 style={headingStyle}>Inline Styles</h2>
     </div>
   );
 }
