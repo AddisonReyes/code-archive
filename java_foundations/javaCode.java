028a067bbb9756373ba516ceb9d5cfb49a680a60
AddisonReyes <addison.amin@gmail.com>
2026-03-13 09:09:03 -0400

    Learning java (again)

---
+++ java_foundations/javaCode.java
@@ -0,0 +1,9 @@
+package java_foundations;
+
+class Start {
+    public static void main(String[] args) {
+        String hello = "Hello World";
+        System.out.println(hello);
+        // Comment
+    }
+}
