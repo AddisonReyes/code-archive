c30b529d63352b5f1b4fbf2575478b4c790f2bf6
AddisonReyes <addison.amin@gmail.com>
2026-03-25 19:23:33 -0400

    Enums challenge

---
+++ Rust/parking_prices/src/main.rs
@@ -0,0 +1,57 @@
+/* Step 1: Define the CarType enum - SUV, Sedan, Coupe */
+enum CarType {
+    SUV,
+    Sedan,
+    Coupe,
+}
+
+/*
+    Step 2: Define the Vehicle enum
+        Car -> Car types
+        Truck -> Cargo Capacity
+        Motorcycle
+*/
+enum Vehicle {
+    Car(CarType),
+    Truck(i32),
+    Motorcycle,
+}
+
+/* Step 3: Write a function to calculate parking rates */
+fn parking_rate(veh: Vehicle) -> i32 {
+    match veh {
+        Vehicle::Car(CarType::SUV) => return 20,
+        Vehicle::Car(CarType::Sedan) => return 15,
+        Vehicle::Car(CarType::Coupe) => return 10,
+        Vehicle::Motorcycle => return 10,
+        Vehicle::Truck(tons) => {
+            if tons <= 10 {
+                return 20;
+            } else {
+                return 25;
+            }
+        }
+    }
+}
+
+fn main() {
+    /* Step 4: Create a vehicle and pass it to the function */
+    let vehicles = [
+        Vehicle::Car(CarType::SUV),
+        Vehicle::Car(CarType::SUV),
+        Vehicle::Car(CarType::Sedan),
+        Vehicle::Car(CarType::Coupe),
+        Vehicle::Car(CarType::Coupe),
+        Vehicle::Car(CarType::Coupe),
+        Vehicle::Truck(67),
+        Vehicle::Truck(20),
+        Vehicle::Truck(5),
+        Vehicle::Motorcycle,
+        Vehicle::Motorcycle,
+    ];
+
+    for veh in vehicles {
+        let rate = parking_rate(veh);
+        print!("{}, ", rate);
+    }
+}
