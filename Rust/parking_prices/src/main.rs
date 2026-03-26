d405216091e14ea8e62da2ed91ef19df74e16bf0
AddisonReyes <addison.amin@gmail.com>
2026-03-25 22:32:34 -0400

    Changing the parking rate function to a method of Vehicle

--- Rust/parking_prices/src/main.rs
+++ Rust/parking_prices/src/main.rs
@@ -18,17 +18,19 @@ enum Vehicle {
 }
 
 /* Step 3: Write a function to calculate parking rates */
-fn parking_rate(veh: Vehicle) -> i32 {
-    match veh {
-        Vehicle::Car(CarType::SUV) => return 20,
-        Vehicle::Car(CarType::Sedan) => return 15,
-        Vehicle::Car(CarType::Coupe) => return 10,
-        Vehicle::Motorcycle => return 10,
-        Vehicle::Truck(tons) => {
-            if tons <= 10 {
-                return 20;
-            } else {
-                return 25;
+impl Vehicle {
+    fn get_parking_rate(&self) -> i32 {
+        match self {
+            Vehicle::Car(CarType::SUV) => return 20,
+            Vehicle::Car(CarType::Sedan) => return 15,
+            Vehicle::Car(CarType::Coupe) => return 10,
+            Vehicle::Motorcycle => return 10,
+            Vehicle::Truck(tons) => {
+                if *tons <= 10 {
+                    return 20;
+                } else {
+                    return 25;
+                }
             }
         }
     }
@@ -51,7 +53,7 @@ fn main() {
     ];
 
     for veh in vehicles {
-        let rate = parking_rate(veh);
+        let rate = veh.get_parking_rate();
         print!("{}, ", rate);
     }
 }
