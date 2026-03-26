62c68e09fdb93aba710512e23f9b4ef7ba262521
AddisonReyes <addison.amin@gmail.com>
2026-03-25 23:14:10 -0400

    second exercise

--- Rust/financial_calculations/src/main.rs
+++ Rust/financial_calculations/src/main.rs
@@ -1,20 +1,34 @@
-mod calculations;
+use num::integer::gcd;
+use num::integer::lcm;
+
+mod interest;
+mod tax;
 
 fn main() {
     let income = 45000.0;
-    let tax = calculations::calculate_income_tax(income);
+    let tax = tax::calculate_income_tax(income);
     println!("Annual Income Tax: ${:.2}", tax);
 
     let principal = 1000.0;
     let rate = 0.05;
     let time = 10.0;
     let n = 4.0;
-    let compound_amount = calculations::calculate_compound_interest(principal, rate, time, n);
+    let compound_amount = interest::calculate_compound_interest(principal, rate, time, n);
     println!("Compound Interest Amount: ${:.2}", compound_amount);
 
-    let simple_amount = calculations::calculate_simple_interest(principal, rate, time);
+    let simple_amount = interest::calculate_simple_interest(principal, rate, time);
     println!("Simple Interest Amount: ${:.2}", simple_amount);
 
-    let tax_rate = calculations::get_tax_rate(income);
+    let tax_rate = tax::get_tax_rate(income);
     println!("Tax Rate: {:.2}%", tax_rate * 100.0);
+
+    // Scenario 2: Using the num crate for GCD and LCM
+    let x = 1234;
+    let y = 9876;
+
+    let result_lsm = lcm(x, y);
+    let result_gcd = gcd(x, y);
+
+    println!("\nGCD => {}", result_gcd);
+    println!("LSM => {}", result_lsm);
 }
--- Rust/financial_calculations/src/tax.rs
+++ Rust/financial_calculations/src/tax.rs
@@ -12,11 +12,3 @@ pub fn get_tax_rate(income: f64) -> f64 {
         0.2
     }
 }
-
-pub fn calculate_compound_interest(principal: f64, rate: f64, time: f64, n: f64) -> f64 {
-    principal * (1.0 + rate / n).powf(n * time)
-}
-
-pub fn calculate_simple_interest(principal: f64, rate: f64, time: f64) -> f64 {
-    principal * rate * time
-}
