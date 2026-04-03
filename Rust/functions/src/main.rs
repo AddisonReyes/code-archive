8680fb454c4ad9b1dd1162e69200d84e61fba057
AddisonReyes <addison.amin@gmail.com>
2026-04-03 19:00:32 -0400

    Reading chapter 3, common programming concepts

--- Rust/functions/src/main.rs
+++ Rust/functions/src/main.rs
@@ -1,11 +1,7 @@
-fn main() {
-    let name = String::from("Addison");
-    greet_user(name);
+use std::io;
 
-    let a = 5;
-    let b = 10;
-    let sum = calculate_sum(a, b);
-    println!("{} + {} = {}", a, b, sum)
+fn greet_user(name: String) {
+    println!("Hello {}, welcome to Rust Programming!", name);
 }
 
 fn calculate_sum(a: i32, b: i32) -> i32 {
@@ -13,6 +9,56 @@ fn calculate_sum(a: i32, b: i32) -> i32 {
     sum // Return -> expressions
 }
 
-fn greet_user(name: String) {
-    println!("Hello {}, welcome to Rust Programming!", name);
+fn get_data(label: &str, mut data: String) -> Option<usize> {
+    println!("{label}");
+    io::stdin()
+        .read_line(&mut data)
+        .expect("Failed to read line.");
+
+    let data: Option<usize> = match data.trim().parse() {
+        Ok(len) => Some(len),
+        Err(_) => None,
+    };
+
+    data
+}
+
+fn vec_gen(value: u8, length: usize) -> Vec<u8> {
+    let arr: Vec<u8> = vec![value; length];
+    arr
+}
+
+fn main() {
+    let name = String::from("Addison");
+    greet_user(name);
+
+    let a = 5;
+    let b = 10;
+    let sum = calculate_sum(a, b);
+    println!("{} + {} = {}\n", a, b, sum);
+
+    let y = {
+        let x = 66;
+        x + 1
+    };
+
+    println!("The value of \'y\' is: {y}");
+
+    loop {
+        let length = String::new();
+        let length = match get_data("\nPlease enter an array length: ", length) {
+            Some(length) => length,
+            None => continue,
+        };
+
+        let value = String::new();
+        let value = match get_data("\nPlease enter a default value: ", value) {
+            Some(value) => value,
+            None => continue,
+        };
+
+        let arr = vec_gen(value as u8, length);
+        println!("\n({length}) Array: {:?}", arr);
+        break;
+    }
 }
--- Rust/variables/src/main.rs
+++ Rust/variables/src/main.rs
@@ -3,12 +3,12 @@ fn main() {
     value = 10;
 
     println!("The value is {}", value);
-    
+
     let x = 66;
     let x = x + 1; // Shadowing
     let x = "Rust programming";
     println!("The value is {}", x);
-    
+
     let small_value: i8 = 100;
     let simple_float: f32 = -700.25;
     println!("The small_value is {}", small_value);
@@ -17,31 +17,30 @@ fn main() {
     // Arrays
     let numbers = [1, 2, 3, 4, 5];
     println!("Element at index 0: {}", numbers[0]);
-    
+
     // Tuples
     let person = ("Alice", 30, 5.4);
     println!("Name: {}", person.0);
     println!("Age: {}", person.1);
     println!("Height: {}", person.2);
-    
+
     // Constants
     const PI: f32 = 3.14;
     println!("PI = {}", PI);
-    
+
     // Constants vs Immutable variable
-        // You have to specify the data type
-        // Don't suports mutable 
-        // Cannot be shadowed
-    
+    // You have to specify the data type
+    // Don't suports mutable
+    // Cannot be shadowed
+
     // const PI: f32 = 6.28; <-- Error
 
     // Strings
-    let mut name = String::from("Zenva"); 
+    let mut name = String::from("Zenva");
     // let mut name = "Zenva"; <-- This is a String Slices (&str) no a String
-    name.push_str(" Academy"); 
-    println!("{}", name); 
+    name.push_str(" Academy");
+    println!("{}", name);
 
     // Use &str when you have a string literal or when you do not need to own the data.
     // Use String when you require a string that you can modify or if you need ownership of the data.
-
 }
