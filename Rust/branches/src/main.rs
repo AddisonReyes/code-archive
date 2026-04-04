67b4f435cdc3c3861b34e443fd496dc6a6c30bf2
AddisonReyes <addison.amin@gmail.com>
2026-04-04 14:12:15 -0400

    Nested loops and branches

---
+++ Rust/branches/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let condition = true;
+
+    let number = if condition { 5 } else { 6 };
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3 or 2");
+    }
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("\nThe result is {result}\n");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {count}\n");
+}
