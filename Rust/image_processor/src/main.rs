e536dc548e390fc46863137afb7e4d1c8cb428a0
AddisonReyes <addison.amin@gmail.com>
2026-04-08 11:12:54 -0400

    Refactoring and resizing

--- Rust/image_processor/src/main.rs
+++ Rust/image_processor/src/main.rs
@@ -1,18 +1,36 @@
-use image::{DynamicImage, GenericImageView, ImageFormat, open};
+use image::{DynamicImage, GenericImageView, ImageFormat, imageops::FilterType, open};
+
+fn resize_image(path: &str, width: u32, height: u32) -> DynamicImage {
+    let img = open(path).expect("Failure to load/open image");
+    img.resize(width, height, FilterType::Lanczos3)
+}
+
+fn save_image(img: &DynamicImage, output_path: &str, format: ImageFormat) {
+    img.save_with_format(output_path, format)
+        .expect("Failed to save image.");
+}
 
 fn main() {
-    let saving_path: String = String::from("assets/sample_img");
+    println!("Image processing");
+
     const IMG_PATH: &str = "assets/sample_img.jpg";
     let img = open(IMG_PATH).expect("Failure to load/open image");
-
     let (width, height) = img.dimensions();
     println!("width: {width}, height: {height}");
 
+    let saving_path: String = String::from("assets/sample_img");
+
     let path = saving_path.clone() + ".png";
-    img.save_with_format(path, ImageFormat::Png)
-        .expect("Failed to save image as PNG");
+    save_image(&img, path.as_str(), ImageFormat::Png);
+    let path = saving_path.clone() + ".webp";
+    save_image(&img, path.as_str(), ImageFormat::WebP);
+
+    let resized_image = resize_image(IMG_PATH, 512, 512);
+    let saving_path: String = String::from("assets/sample_img_RESIZED");
+
+    let path = saving_path.clone() + ".png";
+    save_image(&resized_image, path.as_str(), ImageFormat::Png);
 
     let path = saving_path.clone() + ".webp";
-    img.save_with_format(path, ImageFormat::WebP)
-        .expect("Failed to save image as WEBP");
+    save_image(&resized_image, path.as_str(), ImageFormat::WebP);
 }
