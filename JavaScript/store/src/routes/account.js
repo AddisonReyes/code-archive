c46ad7b51cbf74398028f51b957f3be0e0ecb64b
AddisonReyes <addison.amin@gmail.com>
2026-04-11 19:00:38 -0400

    Displaying items in the account view

--- JavaScript/store/src/routes/account.js
+++ JavaScript/store/src/routes/account.js
@@ -3,6 +3,15 @@ import Router from "express";
 const env = process.env.NODE_ENV || "prod";
 const router = Router();
 
+const items = [
+  { name: "Item 1", description: "description", price: 10 },
+  { name: "Item 2", description: "description", price: 15 },
+  { name: "Item 3", description: "description", price: 20 },
+  { name: "Item 4", description: "description", price: 25 },
+  { name: "Item 5", description: "description", price: 30 },
+  { name: "Item 6", description: "description", price: 35 },
+];
+
 router.get("/", (req, res, next) => {
   if (env === "dev") {
     console.log(`${req.baseUrl || "/"} - ${req.method} :: Account view`);
@@ -14,7 +23,7 @@ router.get("/", (req, res, next) => {
     return;
   }
 
-  res.render("account", { title: "Account", user });
+  res.render("account", { title: "Account", items, user });
 });
 
 router.get("/logout", (req, res, next) => {
