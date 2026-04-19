2f3e7dff1be2b155641803bb9f2ddfeb17c6ebf3
AddisonReyes <addison.amin@gmail.com>
2026-04-19 18:13:47 -0400

    [Rust/Book] Associated functions and another method

--- Rust/rectangles/src/main.rs
+++ Rust/rectangles/src/main.rs
@@ -5,8 +5,19 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn squeare(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -17,11 +28,14 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle::squeare(20);
+
     // println!("rect1 is {:#?}\n", rect1);
     dbg!(&rect1);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    println!("The area of the rect1 is {} square pixels.", rect1.area());
+    println!("The area of the rect2 is {} square pixels.", rect2.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect1? {}", rect1.can_hold(&rect1));
 }
